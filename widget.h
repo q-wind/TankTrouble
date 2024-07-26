73b35682c8dd6bbfba8320f7471597855a9668cc
q_wind <qing-wen@qq.com>
2024-07-27 00:42:59 +0800

    start widget ui, still missing good background img

--- widget.h
+++ widget.h
@@ -2,7 +2,13 @@
 #define WIDGET_H
 
 #include <QWidget>
+#include <QPushButton>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QPaintEvent>
+#include <QResizeEvent>
 
+// Start Widget
 class Widget : public QWidget
 {
     Q_OBJECT
@@ -10,5 +16,31 @@ class Widget : public QWidget
 public:
     Widget(QWidget *parent = nullptr);
     ~Widget();
+
+protected:
+    void paintEvent(QPaintEvent* event) override;
+    void resizeEvent(QResizeEvent* event) override;
+
+private:
+    void SetUI();
+    void SetSignalSlot();
+
+    const int initWidth   = 800;
+    const int initHeight  = 600;
+    const float aspectRadio = 4.0f / 3.0f;
+
+    // Qt中自动内存管理，父窗口析构时会负责销毁
+    QHBoxLayout* startLayout;
+    QPushButton* btnStart;
+
+    QHBoxLayout* modeLayout;
+    QPushButton* btnAIMode;
+    QPushButton* btnLocalMode;
+    QPushButton* btnOnlineMode;
+
+    QVBoxLayout* mainLayout;
+
+private slots:
+    void DoShowGameMode();
 };
 #endif // WIDGET_H
